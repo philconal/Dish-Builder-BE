e86eeed0c9275d1d645da01df5c64ba866dd761e
philconal2411@gmail.com <philconal2411@gmail.com>
2025-08-03 16:01:43 +0700

    init test

---
+++ src/test/java/com/conal/dishbuilder/DishbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conal.dishbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishbuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
