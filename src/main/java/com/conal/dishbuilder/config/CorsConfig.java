49651127ff0374d2c7f3d940e2697aa471ed87ae
philconal2411@gmail.com <philconal2411@gmail.com>
2025-08-03 18:09:53 +0700

    add cors config

---
+++ src/main/java/com/conal/dishbuilder/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.conal.dishbuilder.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://dish-builder-be.onrender.com") // Adjust the origin as needed
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+}
