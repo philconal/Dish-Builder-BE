3b63a02e02e1ab57064af4da47c509fc4982c61b
philconal2411@gmail.com <philconal2411@gmail.com>
2025-08-26 01:18:48 +0700

    update config

---
+++ src/main/java/com/conal/dishbuilder/exception/UnsupportedSortPropertyException.java
@@ -0,0 +1,8 @@
+package com.conal.dishbuilder.exception;
+
+public class UnsupportedSortPropertyException  extends RuntimeException {
+
+    public UnsupportedSortPropertyException (String message) {
+        super(message);
+    }
+}
